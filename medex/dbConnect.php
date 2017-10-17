772e0147fd826960c46d4cb3ca9219e07ed17de7
Fazle Rabbi Ador <aujisti.ador@gmail.com>
2017-10-18 00:19:33 +0600

    scripts edited

--- medex/dbConnect.php
+++ medex/dbConnect.php
@@ -1,8 +1,8 @@
 <?php
 
-	define('HOST','localhost');
-	define('USER','id772002_ador');
-	define('PASS','@ujisti@dor');
-	define('DB','id772002_db_blood_request');
+	define('HOST','xyz');
+	define('USER','xyz');
+	define('PASS','xyz');
+	define('DB','xyz');
 	
 	$con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
